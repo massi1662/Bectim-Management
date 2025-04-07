99c475763555d9bd85533916fff851d672d5d446
massi1662 <massi.ch1234@gmail.com>
2025-04-07 10:52:26 +0100

    maj

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -14,23 +14,29 @@ import Trash from "./pages/Trash";
 import Users from "./pages/Users";
 import TaskDetails from "./pages/TaskDetails";
 import { Toaster } from "sonner";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import { setOpenSidebar } from "./redux/slices/authSlice";
+import clsx from "clsx";
+import { IoClose } from "react-icons/io5";
+import { Fragment, useRef } from "react";
+import { Transition } from "@headlessui/react";
 
 function Layout() {
-  const {user} = useSelector(state=>state.auth)
+  const { user } = useSelector((state) => state.auth);
 
   const location = useLocation();
 
   return user ? (
-    <div className="w-full h-screen flex flex-col md-flex-row">
+    <div className="w-full h-screen flex flex-col md:flex-row">
       <div className="w-1/5 h-screen bg-white sticky top-0 hidden md:block">
         <Sidebar />
       </div>
 
-      {/* Mobile Sidebar */}
+      <MobileSidebar />
       <div className="flex-1 overflow-y-auto">
-        {/* Navbar */}
+        <Navbar />
         <div className="p-4 2xl:px-10">
           <Outlet />
         </div>
@@ -40,6 +46,57 @@ function Layout() {
     <Navigate to="/login" state={{ from: location }} replace />
   );
 }
+
+const MobileSidebar = () => {
+  const { isSidebarOpen } = useSelector((state) => state.auth);
+  const mobileMenuRef = useRef(null);
+  const dispatch = useDispatch();
+
+  const closeSidebar = () => {
+    dispatch(setOpenSidebar(false));
+  };
+
+  return (
+    <>
+      <Transition
+        show={isSidebarOpen}
+        as={Fragment}
+        enter="transition-opacity duration-700"
+        enterFrom="opacity-x-10"
+        enterTo="opacity-x-100"
+        leave="transition-opacity duration-700"
+        leaveFrom="opacity-x-100"
+        leaveTo="opacity-x-0"
+      >
+        {(ref) => (
+          <div
+            ref={(node) => (mobileMenuRef.current = node)}
+            className={clsx(
+              "md:hidden w-full h-full bg-black/40 transition-all duration-700 transform",
+              isSidebarOpen ? "translate-x-0" : "translate-x-full"
+            )}
+            onClick={() => closeSidebar()}
+          >
+            <div className="bg-white w-2/4 h-full ">
+              <div className="w-full flex justify-end px-5 mt-5">
+                <button
+                  onClick={() => closeSidebar()}
+                  className="flex justify-end items-  end"
+                >
+                  <IoClose size={25} />
+                </button>
+              </div>
+
+              <div className="-mt-10">
+                <Sidebar />
+              </div>
+            </div>
+          </div>
+        )}
+      </Transition>
+    </>
+  );
+};
 
 function App() {
   return (
--- Client/src/components/Sidebar.jsx
+++ Client/src/components/Sidebar.jsx
@@ -12,6 +12,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link, NavLink, useLocation } from "react-router-dom";
 import clsx from "clsx";
 
+
 const linkData = [
   {
     label: "Dashboard",
@@ -71,7 +72,7 @@ const Sidebar = () => {
         onClick={closeSidebar}
         className={clsx(
           "w-full lg:w-3/4 flex gap-2 px-3 py-2 rounded-full items-center text-gray-800 text-base hover:bg-[#2564ed2d]",
-          path === el.link.split("/")[0] ? "bg-blue-700 text-white" : ""
+          path === el.link.split("/")[0] ? "bg-blue-500 text-white hover:text-gray-600" : ""
         )}
       >
 
@@ -84,13 +85,13 @@ const Sidebar = () => {
   return (
     <div className="w-full h-full flex flex-col gap-6 p-5">
       <h1 className="flex gap-1 items-center">
-        <p className="bg-blue-600 p-2 rounded-full">
-          <MdOutlineAddTask className="text-white text-2xl font-black" />
+        <p className="bg-blue-500 p-1 rounded-full">
+          <img alt="logo-bectim" src="https://bectim.com/images/logo%20bectim.png" className="text-white w-12 text-2xl font-black" />
         </p>
-        <span className="text-2xl font-bold text-black">BECTIM</span>
+        <span className="text-2xl font-black text-red-900">BECTIM</span>
       </h1>
 
-      <div className="flex-1 flex flex-col gap-y-5 py-8">
+      <div className="flex-1 flex flex-col gap-y-2 py-8">
         {sidebarLinks.map((link) => {
           console.log(link.label);
          return <NavLink el={link} key={link.label} />;
@@ -100,7 +101,7 @@ const Sidebar = () => {
       <div className=''>
         <button className='w-full flex gap-2 p-2 items-center text-lg text-gray-800'>
           <MdSettings />
-          <span>Settings</span>
+          <span>Settings</span>                   
         </button>
       </div>
     </div>
